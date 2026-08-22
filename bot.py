f6d8f484619124733f1d43c5ed942bc66dc65c2a
nikanghafaricom <nikanghafaricom@gmail.com>
2026-08-22 03:52:11 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -101,7 +101,7 @@ class TabdilExchange:
     def __init__(self, api_key: str, secret: str):
         self.api_key = api_key
         self.secret = secret
-        self.base_url = "https://api.tabdil.org"
+        self.base_url = "https://api.tabdeal.org"
         self.headers = {
             "X-API-KEY": self.api_key,
             "Content-Type": "application/json"
